81eff8aaf732c8a1a80c50b85eceaa10b16a659e
Daniel <ddoherty6@gmail.com>
2022-03-31 16:25:08 -0700

    added form and resume

--- client/src/components/Contact/index.js
+++ client/src/components/Contact/index.js
@@ -1,11 +1,45 @@
 import React , { useState } from 'react';
+//import validateEmail  from '../../utils'; issues importing this file - small enough to define here until bug can be fixed
+
+const validateEmail = function(email) {
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
+        return (true)
+    }
+    
+    return (false)
+}
 
 function Contact() {
     const [formState, setFormState] = useState({name: '', email: '', message: ''});
     const { name, email, message } = formState;
+    const [errorMessage, setErrorMessage] = useState('');
 
     const handleChange = function(event) {
-        setFormState({...formState, [event.target.name]: event.target.value});
+
+        // validate inputs, set error message for invalid ipnuts onChange
+        if (event.target.name === 'email') {
+            const isValid = validateEmail(event.target.value);
+        
+            if (!isValid) {
+                setErrorMessage('Your email is invalid.');
+            } else {
+                setErrorMessage('');
+            }
+
+        } else {
+            if (!event.target.value.length) {
+                setErrorMessage(`${event.target.name} is required.`);
+              } else {
+                setErrorMessage('');
+              }
+        }
+        
+        // validation is successfull
+        if (!errorMessage) {
+            setFormState({ ...formState, [event.target.name]: event.target.value });
+        }
+
+        
     }
 
     const handleSubmit = function(event) {
@@ -14,24 +48,29 @@ function Contact() {
     }
 
     return (
-        <section>
-      <h1>Contact me</h1>
-      <form id="contact" onSubmit={handleSubmit}>
-        <div>
-            <label htmlFor="name">Name:</label>
-            <input type="text" name="name" defaultValue={name} onChange={handleChange} />
-        </div>
-        <div>
-            <label htmlFor="email">Email address:</label>
-            <input type="email" name="email" defaultValue={email} onChange={handleChange} />
-        </div>
-        <div>
-            <label htmlFor="message">Message:</label>
-            <textarea name="message" rows="5" defaultValue={message} onChange={handleChange} />
-        </div>
-        <button type="submit">Submit</button>
-      </form>
-    </section>
+        <section id="contact">
+            <h1>Contact Me</h1>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label htmlFor="name">Name:</label>
+                    <p><input type="text" name="name" defaultValue={name} onChange={handleChange} /></p>
+                </div>
+                <div>
+                    <label htmlFor="email">Email address:</label>
+                    <p><input type="email" name="email" defaultValue={email} onChange={handleChange} /></p>
+                </div>
+                <div>
+                    <label htmlFor="message">Message:</label>
+                    <p><textarea name="message" rows="5" defaultValue={message} onChange={handleChange} /></p>
+                </div>
+                {errorMessage && (
+                    <div>
+                        <p className="error-text">{errorMessage}</p>
+                    </div>
+                )}
+                <button type="submit">Submit</button>
+            </form>
+        </section>
     );
 }
 
--- client/src/components/Resume/index.js
+++ client/src/components/Resume/index.js
@@ -2,9 +2,24 @@ function Resume() {
    
     return (
         <main id="resume">
-            <ul>
-                <li>resi</li>
-            </ul>
+            <div>
+                <h1><a href="./assets/Resume.pdf" target="_blank">Resume</a></h1>
+                <h2>Front end skills</h2>
+                <ul>
+                    <li>HTML/CSS</li>
+                    <li>Javascript/jQuery</li>
+                    <li>Boostrap/Materialize</li>
+                </ul>
+                <h2>Back end skills</h2>
+                <ul>
+                    <li>SQL and noSQL</li>
+                    <li>Git/Heroku/MongoDB Atlas</li>
+                    <li>NodeJS</li>
+                    <li>ExpressJS</li>
+                    <li>ReactJS</li>
+                    <li>ExpressJS</li>
+                </ul>
+            </div>
             
         </main>
     );
