334bdae2bc968221038d62df644a1d9577efceaf
Daniel <ddoherty6@gmail.com>
2022-03-31 13:29:21 -0700

    added contact form

--- client/src/components/Contact/index.js
+++ client/src/components/Contact/index.js
@@ -1,11 +1,37 @@
+import React , { useState } from 'react';
+
 function Contact() {
+    const [formState, setFormState] = useState({name: '', email: '', message: ''});
+    const { name, email, message } = formState;
+
+    const handleChange = function(event) {
+        setFormState({...formState, [event.target.name]: event.target.value});
+    }
+
+    const handleSubmit = function(event) {
+        event.preventDefault();
+        console.log(formState); // leaving as front-end only for now
+    }
+
     return (
-        <form id="contact">
-            <ul>
-                <li>COntact</li>
-                <li>My guy</li>
-            </ul>
-        </form>
+        <section>
+      <h1>Contact me</h1>
+      <form id="contact" onSubmit={handleSubmit}>
+        <div>
+            <label htmlFor="name">Name:</label>
+            <input type="text" name="name" defaultValue={name} onChange={handleChange} />
+        </div>
+        <div>
+            <label htmlFor="email">Email address:</label>
+            <input type="email" name="email" defaultValue={email} onChange={handleChange} />
+        </div>
+        <div>
+            <label htmlFor="message">Message:</label>
+            <textarea name="message" rows="5" defaultValue={message} onChange={handleChange} />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    </section>
     );
 }
 
