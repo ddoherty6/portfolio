cd8048205cd19b9246c59862305942c8f26660e6
Daniel <ddoherty6@gmail.com>
2022-03-31 17:19:01 -0700

    add readme

---
+++ client/src/components/Header/index.js
@@ -0,0 +1,12 @@
+import Navgation from '../Navigation';
+
+function Header({ nav, setNav }) {
+    return (
+        <header>
+            <div><h1> Dan Doherty</h1></div>
+            <Navgation nav={nav} setNav={setNav}/>
+        </header>
+    );
+}
+
+export default Header;
