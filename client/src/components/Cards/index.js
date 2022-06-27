2aecc93a1b4ba48a792e02812edb72d3e9fc1b01
klem <ddoherty6@gmail.com>
2022-06-27 15:55:13 -0700

    begin drag-drop set-up

--- client/src/components/Cards/index.js
+++ client/src/components/Cards/index.js
@@ -1,20 +1,18 @@
 import Card from 'react-bootstrap/Card';
-import Row from 'react-bootstrap/Row';
+
 import './style.css';
 
-function Cards(cardInfo) {
+function Cards({cardInfo}) {
     return (
-        <Row>
-            <Card>
-                <Card.Img src={cardInfo.img} alt=""/>
-                <Card.Body>
-                    <Card.Title>{cardInfo.title}</Card.Title>
-                    <Card.Text>{cardInfo.text}</Card.Text>
-                    <Card.Link href={cardInfo.url}>deployment</Card.Link>
-                    <Card.Link href={cardInfo.repo}>repo</Card.Link>
-                </Card.Body>
-            </Card>
-        </Row>
+        <Card>
+            {/* <Card.Img src={cardInfo.img} alt=""/> */}
+            <Card.Body>
+                <Card.Title>{cardInfo.title}</Card.Title>
+                {/* <Card.Text>{cardInfo.text}</Card.Text> */}
+                <Card.Link href={cardInfo.url}>deployment</Card.Link>
+                <Card.Link href={cardInfo.repo}>repo</Card.Link>
+            </Card.Body>
+        </Card>
     );
 }
 
--- client/src/components/DragDrop/index.js
+++ client/src/components/DragDrop/index.js
@@ -1,14 +1,16 @@
 import Cards from '../Cards';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import data from '../../data';
 
 function DragDrop() {
     return (
-        <>
-            <div className="dragPool"> {data.map((datum) => {
-                return <Cards cardInfo={datum}/>;
-            })} </div>
-            <div className="dropBoard"></div>
-        </>
+        <Row>
+            <Col className="dropBoard col-3"></Col>
+            <Col className="dragPool"> {data.map((datum, i) => {
+                return <Cards key={i} cardInfo={datum}/>;
+            })} </Col>
+        </Row>
     );
 }
 
